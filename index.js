b6d460028cb19286a97da5ee8c4b5c289e0fa03c
Abhishek Borar <abhishek@musicmonk.in>
2017-10-11 22:47:48 +0530

    adding static handler

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const queryHandler = require(__dirname + '/queryHandler');
 
 const PORT = process.env.API_PORT || process.env.PORT || 8080;
 
+app.use('/', express.static(__dirname + '/docs'));
+
 app.get('/providers', (req, res) => {
 	queryHandler.execute(queryHandler.parse(req.query), (err, rows) => {
 		if(err) res.status(500).send(err);
